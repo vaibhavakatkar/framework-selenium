8bc914cbd1819b4d495bab2f88e43e4b5f95f2df
Vaibhav Katkar <vkatkar@inferyx.com>
2021-02-19 14:58:55 +0530

    TestMain commented

--- src/test/java/com/inferyx/framework/selenium/TestMain.java
+++ src/test/java/com/inferyx/framework/selenium/TestMain.java
@@ -1,20 +1,18 @@
-package com.inferyx.framework.selenium;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.testng.TestNG;
-
-public class TestMain {
-
-	public static void main(String[] args) {
-		String currentDir = System.getProperty("user.dir");
-        System.out.println("Current dir using System:" +currentDir);
-			    List<String> file = new ArrayList<String>();
-			    file.add( System.getProperty("user.dir")+"/src/main/resources/testng.xml");
-			    TestNG testNG = new TestNG();
-			    testNG.setTestSuites(file);
-			    testNG.run();
-	}
-
-}
+/*
+ * package com.inferyx.framework.selenium;
+ * 
+ * import java.util.ArrayList; import java.util.List;
+ * 
+ * import org.testng.TestNG;
+ * 
+ * public class TestMain {
+ * 
+ * public static void main(String[] args) { String currentDir =
+ * System.getProperty("user.dir");
+ * System.out.println("Current dir using System:" +currentDir); List<String>
+ * file = new ArrayList<String>(); file.add(
+ * System.getProperty("user.dir")+"/src/main/resources/testng.xml"); TestNG
+ * testNG = new TestNG(); testNG.setTestSuites(file); testNG.run(); }
+ * 
+ * }
+ */
